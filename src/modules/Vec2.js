d016e4164d1a99a390a5344f7cd0d36e937de80f
Christian <cisr918@gmail.com>
2017-02-05 03:58:34 -0500

    Implement vector

--- src/modules/Vec2.js
+++ src/modules/Vec2.js
@@ -3,25 +3,25 @@
 // Written by Rahat Ahmed (http://rahatah.me/d).
 
 function Vec2(x, y) {
-	this.x = x;
-	this.y = y;
+    this.x = x;
+    this.y = y;
 }
 
 Vec2.prototype.add = function(x, y) {
-	if (x instanceof Vec2) {
-		this.x += x.x;
-		this.y += x.y;
-	} else {
-		this.x += x;
-		this.y += y;
-	}
-	return this;
+    if (x instanceof Vec2) {
+	this.x += x.x;
+	this.y += x.y;
+    } else {
+	this.x += x;
+	this.y += y;
+    }
+    return this;
 };
 
 Vec2.prototype.add2 = function(d, m) {
-	this.x += d.x * m;
-	this.y += d.y * m;
-	return this;
+    this.x += d.x * m;
+    this.y += d.y * m;
+    return this;
 }
 
 Vec2.prototype.sub = function(x, y) {
@@ -36,25 +36,25 @@ Vec2.prototype.sub = function(x, y) {
 };
 
 Vec2.prototype.sub2 = function(d, m) {
-	this.x -= d.x * m;
-	this.y -= d.y * m;
-	return this;
+    this.x -= d.x * m;
+    this.y -= d.y * m;
+    return this;
 };
 
 Vec2.prototype.angle = function(a) {
-	return Math.atan2(a.x, a.y);
+    return Math.atan2(a.x, a.y);
 };
 
 Vec2.prototype.clone = function() {
-	return new Vec2(this.x, this.y);
+    return new Vec2(this.x, this.y);
 };
 
 Vec2.prototype.dist = function(d) {
-	return d.x * d.x + d.y * d.y;
+    return d.x * d.x + d.y * d.y;
 };
 
 Vec2.prototype.sqDist = function(d) {
-	return Math.sqrt(d.x * d.x + d.y * d.y);
+    return Math.sqrt(d.x * d.x + d.y * d.y);
 };
 
 Vec2.prototype.length = function() {
@@ -62,13 +62,13 @@ Vec2.prototype.length = function() {
 };
 
 Vec2.prototype.normalize = function() {
-	return this.scale(1/this.length());
+    return this.scale(1/this.length());
 };
 
 Vec2.prototype.scale = function(scaleX, scaleY) {
-	this.x *= scaleX;
-	this.y *= scaleY || scaleX;
-	return this;
+    this.x *= scaleX;
+    this.y *= scaleY || scaleX;
+    return this;
 };
 
 module.exports = Vec2;
