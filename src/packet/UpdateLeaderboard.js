bc1e93f3f146f199b121ce134fc69bd0eb39dda3
Agarian <Agarian@users.noreply.github.com>
2017-04-09 08:42:45 -0400

    Fixes board command

--- src/packet/UpdateLeaderboard.js
+++ src/packet/UpdateLeaderboard.js
@@ -12,7 +12,7 @@ UpdateLeaderboard.prototype.build = function(protocol) {
     switch (this.leaderboardType) {
         case 48:
             // UserText
-            return this.buildUserText(protocol);
+            return protocol < 13 ? this.buildUserText(protocol) : this.buildUserText13();
         case 49:
             // FFA
             if (protocol < 6) return this.buildFfa5();
@@ -27,7 +27,7 @@ UpdateLeaderboard.prototype.build = function(protocol) {
             return null;
     }
 }
-// UserText
+// UserText prorocols 5/6/11
 UpdateLeaderboard.prototype.buildUserText = function(protocol) {
     var writer = new BinaryWriter();
     writeCount(writer, 0x31, this.leaderboard.length);
@@ -36,6 +36,16 @@ UpdateLeaderboard.prototype.buildUserText = function(protocol) {
         if (protocol < 11) writer.writeUInt32(0);
         if (protocol < 6) writer.writeStringZeroUnicode(item);
         else writer.writeStringZeroUtf8(item);
+    }
+    return writer.toBuffer();
+};
+// UserText protocol 13
+UpdateLeaderboard.prototype.buildUserText13 = function () {
+    var writer = new BinaryWriter();
+    writer.writeUInt8(0x33);
+    for (var i = 0; i < this.leaderboard.length; i++) {
+        writer.writeUInt8(0x02);
+        writer.writeStringZeroUtf8(this.leaderboard[i]);
     }
     return writer.toBuffer();
 };
