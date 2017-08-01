921b9ed829281c16c2d905b3faaeed51aaa093dc
AlexHGaming <lightweightagarioclan@gmail.com>
2017-08-01 18:19:53 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ process.on('uncaughtException', function (err) {
     process.exit(1);
 });
 
-// Run Ogar
+// Run MultiOgar-Edited
 var gameServer = new GameServer();
 Logger.info("\u001B[1m\u001B[32mMultiOgar-Edited " + gameServer.version + "\u001B[37m - An open source multi-protocol ogar server\u001B[0m");
 
