9e8880bb380e82d4c55e6fbf10804952389f736c
Fantasy <fantasyagario123@gmail.com>
2017-09-17 22:20:29 +0100

    Update win-Install_Dep.bat

--- run/win-Install_Dep.bat
+++ run/win-Install_Dep.bat
@@ -4,5 +4,5 @@ echo.
 echo.
 echo Successfully installed all dependencies!
 echo Please don't touch folder "node-modules".
-echo It is the folder where dependencies are installed.
+echo It is the folder where dependencies, that are reqired to run Multiogar-Edited, are installed.
 pause >nul
