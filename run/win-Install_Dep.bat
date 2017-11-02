7539c00ce41b5321b5ce5fbf08acacf6d2d2973f
Ronit Sajeey <rsajeey@hotmail.com>
2017-11-02 22:54:00 +0000

    Update win-Install_Dep.bat

--- run/win-Install_Dep.bat
+++ run/win-Install_Dep.bat
@@ -3,6 +3,6 @@ npm install
 echo.
 echo.
 echo Successfully installed all dependencies!
-echo Please don't touch folder "node-modules".
-echo It is the folder where dependencies, that are required to run Multiogar-Edited, are installed.
+echo Please don't touch the "node-modules" folder.
+echo It is the folder where dependencies, that are required to run MultiOgar-Edited, are installed.
 pause >nul
