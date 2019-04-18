1c80eddef734d5c3d3d8b3a37bf1a111a96cb02d
B0RYS <b0ry5.dev@gmail.com>
2019-04-18 19:37:35 +0100

    Convert EjectedMass.js to es6 syntax.

--- src/entity/EjectedMass.js
+++ src/entity/EjectedMass.js
@@ -1,25 +1,22 @@
 var Cell = require('./Cell');
 
-function EjectedMass() {
-    Cell.apply(this, Array.prototype.slice.call(arguments));
-
-    this.cellType = 3;
+class EjectedMass extends Cell {
+    constructor(gameServer, owner, position, size) {
+        super(gameServer, owner, position, size);
+        this.cellType = 3;
+    }
+    // Main Functions
+    onAdd(gameServer) {
+        // Add to list of ejected mass
+        gameServer.nodesEjected.push(this);
+    }
+    onRemove(gameServer) {
+        // Remove from list of ejected mass
+        var index = gameServer.nodesEjected.indexOf(this);
+        if (index != -1) {
+            gameServer.nodesEjected.splice(index, 1);
+        }
+    }
 }
 
 module.exports = EjectedMass;
-EjectedMass.prototype = new Cell();
-
-// Main Functions
-
-EjectedMass.prototype.onAdd = function (gameServer) {
-    // Add to list of ejected mass
-    gameServer.nodesEjected.push(this);
-};
-
-EjectedMass.prototype.onRemove = function (gameServer) {
-    // Remove from list of ejected mass
-    var index = gameServer.nodesEjected.indexOf(this);
-    if (index != -1) {
-        gameServer.nodesEjected.splice(index, 1);
-    }
-};
