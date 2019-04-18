c575c065cedbc4d74602be2f33e56e7c23cae961
B0RYS <b0ry5.dev@gmail.com>
2019-04-18 19:42:16 +0100

    Convet FFA.js to es6 syntax

--- src/gamemodes/FFA.js
+++ src/gamemodes/FFA.js
@@ -1,38 +1,37 @@
 var Mode = require('./Mode');
 
-function FFA() {
-    Mode.apply(this, Array.prototype.slice.call(arguments));
-
-    this.ID = 0;
-    this.name = "Free For All";
-    this.specByLeaderboard = true;
+class FFA extends Mode {
+    constructor() {
+        super();
+        this.ID = 0;
+        this.name = "Free For All";
+        this.specByLeaderboard = true;
+    }
+    // Gamemode Specific Functions
+    onPlayerSpawn(gameServer, player) {
+        player.color = gameServer.getRandomColor();
+        // Spawn player
+        gameServer.spawnPlayer(player, gameServer.randomPos());
+    }
+    updateLB(gameServer, lb) {
+        gameServer.leaderboardType = this.packetLB;
+        for (var i = 0, pos = 0; i < gameServer.clients.length; i++) {
+            var player = gameServer.clients[i].playerTracker;
+            if (player.isRemoved || !player.cells.length ||
+                player.socket.isConnected == false || (!gameServer.config.minionsOnLeaderboard && player.isMi))
+                continue;
+            for (var j = 0; j < pos; j++)
+                if (lb[j]._score < player._score)
+                    break;
+            lb.splice(j, 0, player);
+            pos++;
+        }
+        this.rankOne = lb[0];
+    }
 }
 
 module.exports = FFA;
 FFA.prototype = new Mode();
 
-// Gamemode Specific Functions
 
-FFA.prototype.onPlayerSpawn = function(gameServer, player) {
-    player.color = gameServer.getRandomColor();
-    // Spawn player
-    gameServer.spawnPlayer(player, gameServer.randomPos());
-};
 
-FFA.prototype.updateLB = function(gameServer, lb) {
-    gameServer.leaderboardType = this.packetLB;
-
-    for (var i = 0, pos = 0; i < gameServer.clients.length; i++) {
-        var player = gameServer.clients[i].playerTracker;
-        if (player.isRemoved || !player.cells.length ||
-            player.socket.isConnected == false || (!gameServer.config.minionsOnLeaderboard && player.isMi))
-            continue;
-
-        for (var j = 0; j < pos; j++)
-            if (lb[j]._score < player._score) break;
-
-        lb.splice(j, 0, player);
-        pos++;
-    }
-    this.rankOne = lb[0];
-};
