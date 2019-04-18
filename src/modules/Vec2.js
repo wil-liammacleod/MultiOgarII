b52d8546d16c2145ec10dbf6a128bb1b9006d5be
B0RYS <b0ry5.dev@gmail.com>
2019-04-18 21:47:23 +0100

    Convert Vec2.js to es6 syntax

--- src/modules/Vec2.js
+++ src/modules/Vec2.js
@@ -2,58 +2,52 @@
 // Based on the Vector2 class from LibGDX.
 // Written by Rahat Ahmed (http://rahatah.me/d).
 
-function Vec2(x, y) {
-    this.x = x;
-    this.y = y;
-}
-
-Vec2.prototype.add = function(d, m) {
-    this.x += d.x * m;
-    this.y += d.y * m;
-    return this;
-}
-
-Vec2.prototype.sub = function(x, y) {
-    if (x instanceof Vec2) {
-        this.x -= x.x;
-        this.y -= x.y;
-    } else {
-        this.x -= x;
-        this.y -= y;
+class Vec2 {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
     }
-    return this;
-};
-
-Vec2.prototype.sub2 = function(d, m) {
-    this.x -= d.x * m;
-    this.y -= d.y * m;
-    return this;
-};
-
-Vec2.prototype.angle = function() {
-    return Math.atan2(this.x, this.y);
-};
-
-Vec2.prototype.clone = function() {
-    return new Vec2(this.x, this.y);
-};
-
-Vec2.prototype.dist = function() {
-    return ~~this.x * ~~this.x + ~~this.y * ~~this.y;
-};
-
-Vec2.prototype.sqDist = function() {
-    return Math.sqrt(this.dist());
-};
-
-Vec2.prototype.normalize = function() {
-    return this.scale(1/this.sqDist());
-};
-
-Vec2.prototype.scale = function(scaleX, scaleY) {
-    this.x *= scaleX;
-    this.y *= scaleY || scaleX;
-    return this;
-};
+    add(d, m) {
+        this.x += d.x * m;
+        this.y += d.y * m;
+        return this;
+    }
+    sub(x, y) {
+        if (x instanceof Vec2) {
+            this.x -= x.x;
+            this.y -= x.y;
+        }
+        else {
+            this.x -= x;
+            this.y -= y;
+        }
+        return this;
+    }
+    sub2(d, m) {
+        this.x -= d.x * m;
+        this.y -= d.y * m;
+        return this;
+    }
+    angle() {
+        return Math.atan2(this.x, this.y);
+    }
+    clone() {
+        return new Vec2(this.x, this.y);
+    }
+    dist() {
+        return ~~this.x * ~~this.x + ~~this.y * ~~this.y;
+    }
+    sqDist() {
+        return Math.sqrt(this.dist());
+    }
+    normalize() {
+        return this.scale(1 / this.sqDist());
+    }
+    scale(scaleX, scaleY) {
+        this.x *= scaleX;
+        this.y *= scaleY || scaleX;
+        return this;
+    }
+}
 
 module.exports = Vec2;
