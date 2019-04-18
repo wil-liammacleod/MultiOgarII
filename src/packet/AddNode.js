d74d860987cdc9a362f2cb13f1c129d97045d7fc
B0RYS <b0ry5.dev@gmail.com>
2019-04-18 21:48:27 +0100

    Convert AddNode.js to es6 syntax

--- src/packet/AddNode.js
+++ src/packet/AddNode.js
@@ -1,15 +1,16 @@
 var BinaryWriter = require("./BinaryWriter");
 
-function AddNode(playerTracker, item) {
-    this.playerTracker = playerTracker;
-    this.item = item;
+class AddNode {
+    constructor(playerTracker, item) {
+        this.playerTracker = playerTracker;
+        this.item = item;
+    }
+    build(protocol) {
+        var writer = new BinaryWriter();
+        writer.writeUInt8(0x20); // Packet ID
+        writer.writeUInt32((this.item.nodeId ^ this.playerTracker.scrambleId) >>> 0);
+        return writer.toBuffer();
+    }
 }
 
 module.exports = AddNode;
-
-AddNode.prototype.build = function (protocol) {
-    var writer = new BinaryWriter();
-    writer.writeUInt8(0x20);                      // Packet ID
-    writer.writeUInt32((this.item.nodeId ^ this.playerTracker.scrambleId) >>> 0);
-    return writer.toBuffer();
-};
