51b3c2d897070414b142431d107dcf88b5f0a998
B0RYS <b0ry5.dev@gmail.com>
2019-04-18 22:08:17 +0100

    Convert UpdatePosition.js to es6 syntax

--- src/packet/UpdatePosition.js
+++ src/packet/UpdatePosition.js
@@ -1,19 +1,21 @@
 var BinaryWriter = require("./BinaryWriter");
 
-function UpdatePosition(playerTracker, x, y, scale) {
-    this.playerTracker = playerTracker,
-    this.x = x;
-    this.y = y;
-    this.scale = scale;
-}
+class UpdatePosition {
+    constructor(playerTracker, x, y, scale) {
+        this.playerTracker = playerTracker,
+            this.x = x;
+        this.y = y;
+        this.scale = scale;
+    }
+
+    build(protocol) {
+        var writer = new BinaryWriter();
+        writer.writeUInt8(0x11);
+        writer.writeFloat(this.x + this.playerTracker.scrambleX);
+        writer.writeFloat(this.y + this.playerTracker.scrambleY);
+        writer.writeFloat(this.scale);
+        return writer.toBuffer();
+    };
+};
 
 module.exports = UpdatePosition;
-
-UpdatePosition.prototype.build = function (protocol) {
-    var writer = new BinaryWriter();
-    writer.writeUInt8(0x11);
-    writer.writeFloat(this.x + this.playerTracker.scrambleX);
-    writer.writeFloat(this.y + this.playerTracker.scrambleY);
-    writer.writeFloat(this.scale);
-    return writer.toBuffer();
-};
