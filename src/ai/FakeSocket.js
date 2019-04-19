305aa6aebd00222ff66036b436b06385192a2c14
B0RYS <b0ry5.dev@gmail.com>
2019-04-19 02:13:23 +0100

    Tidy FakeSocket.js

--- src/ai/FakeSocket.js
+++ src/ai/FakeSocket.js
@@ -1,16 +1,16 @@
-// A fake socket for bot players
 class FakeSocket {
     constructor(server) {
         this.server = server;
         this.isCloseRequest = false;
-    }
+    };
+    
     sendPacket(packet) {
-        // Fakes sending a packet
         return;
-    }
+    };
+
     close(error) {
         this.isCloseRequest = true;
-    }
-}
+    };
+};
 
 module.exports = FakeSocket;
