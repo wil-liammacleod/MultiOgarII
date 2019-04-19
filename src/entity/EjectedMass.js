a6ecbfc99f26306f41fffda49bee42113346b526
B0RYS <b0ry5.dev@gmail.com>
2019-04-19 05:05:33 +0100

    Tidy EjectedMass.js

--- src/entity/EjectedMass.js
+++ src/entity/EjectedMass.js
@@ -1,22 +1,21 @@
-var Cell = require('./Cell');
+const Cell = require('./Cell');
 
 class EjectedMass extends Cell {
     constructor(server, owner, position, size) {
         super(server, owner, position, size);
         this.type = 3;
-    }
-    // Main Functions
+    };
+
     onAdd(server) {
-        // Add to list of ejected mass
         server.nodesEjected.push(this);
-    }
+    };
+
     onRemove(server) {
-        // Remove from list of ejected mass
-        var index = server.nodesEjected.indexOf(this);
+        const index = server.nodesEjected.indexOf(this);
         if (index != -1) {
             server.nodesEjected.splice(index, 1);
-        }
-    }
-}
+        };
+    };
+};
 
 module.exports = EjectedMass;
