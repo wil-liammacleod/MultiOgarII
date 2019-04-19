011fc83df361cfff0c764b7fcadcb168f41eb86d
B0RYS <b0ry5.dev@gmail.com>
2019-04-19 05:06:39 +0100

    Tidy Food.js

--- src/entity/Food.js
+++ src/entity/Food.js
@@ -1,26 +1,25 @@
-var Cell = require('./Cell');
+const Cell = require('./Cell');
 
 class Food extends Cell {
     constructor(server, owner, position, size) {
         super(server, owner, position, size);
         this.type = 1;
         this.overrideReuse = false;
-    }
-    // Main Functions
+    };
+
     onAdd(server) {
         server.nodesFood.push(this);
-    }
+    };
+
     onRemove(server) {
-        // Remove from list of foods
-        var index = server.nodesFood.indexOf(this);
+        const index = server.nodesFood.indexOf(this);
         if (index != -1) {
             server.nodesFood.splice(index, 1);
-        }
-        ;
-        // Respawn
+        };
+
         if (!this.overrideReuse)
             server.spawnFood();
-    }
-}
+    };
+};
 
 module.exports = Food;
