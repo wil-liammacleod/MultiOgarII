b6ac0beb46e3d5fcc0d459770d95975c8b487fa5
B0RYS <b0ry5.dev@gmail.com>
2019-04-19 05:38:36 +0100

    Tidy PlayerCell.js

--- src/entity/PlayerCell.js
+++ src/entity/PlayerCell.js
@@ -6,36 +6,34 @@ class PlayerCell extends Cell {
         super(server, owner, position, size);
         this.type = 0;
         this._canRemerge = false;
-    }
-    // Main Functions
+    };
+
     canEat(cell) {
-        return true; // player cell can eat anyone
-    }
+        return true; 
+    };
+    
     getSpeed(dist) {
-        var speed = 2.2 * Math.pow(this._size, -0.439);
-        speed *= 40 * this.server.config.playerSpeed;
-        return Math.min(dist, speed) / dist;
-    }
+        return Math.min(dist, 2.2 * Math.pow(this._size, -0.45) * 40) / dist;
+    };
+
     onAdd(server) {
-        // Add to player nodes list
         this.color = this.owner.color;
         this.owner.cells.push(this);
         this.owner.socket.packetHandler.sendPacket(new Packet.AddNode(this.owner, this));
         this.server.nodesPlayer.unshift(this);
-        // Gamemode actions
         server.mode.onCellAdd(this);
     }
     onRemove(server) {
-        // Remove from player cell list
-        var index = this.owner.cells.indexOf(this);
+        const index = this.owner.cells.indexOf(this);
         if (index != -1)
             this.owner.cells.splice(index, 1);
         index = this.server.nodesPlayer.indexOf(this);
+
         if (index != -1)
             this.server.nodesPlayer.splice(index, 1);
-        // Gamemode actions
+
         server.mode.onCellRemove(this);
-    }
-}
+    };
+};
 
 module.exports = PlayerCell;
