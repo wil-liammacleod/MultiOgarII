df880bbb5c1f7cc5f33a784a2f12b3b90d306ef3
B0RYS <b0ry5.dev@gmail.com>
2019-04-19 05:05:13 +0100

    Tidy up Cell.js

--- src/entity/Cell.js
+++ src/entity/Cell.js
@@ -1,63 +1,72 @@
-var Vec2 = require('../modules/Vec2');
+const Vec2 = require('../modules/Vec2');
 
 class Cell {
     constructor(server, owner, position, size) {
         this.server = server;
-        this.owner = owner; // playerTracker that owns this cell
+        this.owner = owner; // Client that owns this cell
         this.color = { r: 0, g: 0, b: 0 };
         this.radius = 0;
         this._size = 0;
         this._mass = 0;
         this.type = -1; // 0 = Player Cell, 1 = Food, 2 = Virus, 3 = Ejected Mass
-        this.isSpiked = false; // If true, then this cell has spikes around it
+        this.isVirus = false; // If true, then this cell has spikes around it
         this.isAgitated = false; // If true, then this cell has waves on it's outline
-        this.killedBy = null; // Cell that ate this cell
+        this.killer = null; // Cell that ate this cell
         this.isMoving = false; // Indicate that cell is in boosted mode
         this.boostDistance = 0;
-        this.boostDirection = new Vec2(1, 0);
+        this.boostDirection = new Vec2(0, 0);
+        
         if (this.server) {
-            this.tickOfBirth = this.server.tickCounter;
+            this.createdAt = this.server.ticks;
             this.nodeId = this.server.lastNodeId++ >> 0;
             if (size)
                 this.setSize(size);
             if (position)
                 this.position = new Vec2(position.x, position.y);
-        }
-    }
+        };
+    };
+
     // Fields not defined by the constructor are considered private and need a getter/setter to access from a different class
     setSize(size) {
         this._size = size;
         this.radius = size * size;
         this._mass = this.radius / 100;
-    }
-    // by default cell cannot eat anyone
+    };
+
+    // By default cell cannot eat anyone
     canEat(cell) {
         return false;
     }
+
     // Returns cell age in ticks for specified game tick
     getAge() {
-        return this.server.tickCounter - this.tickOfBirth;
+        return this.server.ticks - this.createdAt;
     }
     // Called to eat prey cell
     onEat(prey) {
         if (!this.server.config.playerBotGrow) {
             if (this._size >= 250 && prey._size <= 41 && prey.type == 0)
                 prey.radius = 0; // Can't grow from players under 17 mass
-        }
-        this.setSize(Math.sqrt(this.radius + prey.radius));
-    }
+        };
+
+        return this.setSize(Math.sqrt(this.radius + prey.radius));
+    };
+
+    // Boost cell
     setBoost(distance, angle) {
         this.boostDistance = distance;
         this.boostDirection = new Vec2(Math.sin(angle), Math.cos(angle));
         this.isMoving = true;
         if (!this.owner) {
-            var index = this.server.movingNodes.indexOf(this);
+            const index = this.server.movingNodes.indexOf(this);
             if (index < 0)
                 this.server.movingNodes.push(this);
-        }
-    }
+        };
+    };
+
+    // Prevent cell from crossing the border
     checkBorder(b) {
-        var r = this._size / 2;
+        const r = this._size / 2;
         if (this.position.x < b.minx + r || this.position.x > b.maxx - r) {
             this.boostDirection.scale(-1, 1); // reflect left-right
             this.position.x = Math.max(this.position.x, b.minx + r);
@@ -69,9 +78,11 @@ class Cell {
             this.position.y = Math.min(this.position.y, b.maxy - r);
         }
     }
-    onEaten(hunter) { }
-    onAdd(server) { }
-    onRemove(server) { }
+
+    // Misc event functions
+    onEaten(hunter) {};
+    onAdd(server) {};
+    onRemove(server) {};
 }
 
 module.exports = Cell;
